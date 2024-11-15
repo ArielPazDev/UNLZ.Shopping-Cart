c27011d8089a42983ca1549b6e2bd7de34147f02
ArielPazDev <contacto.arielpaz.dev@gmail.com>
2024-11-15 17:44:56 -0300

    feat (controllers): add HomeController

---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HomeController() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.setHeader("Content-Type", "text/plain; charset=UTF-8");
+		
+    	PrintWriter print = response.getWriter();
+		
+    	print.append("request.getContextPath(): ").append(request.getContextPath() + "\n");
+		print.append("request.getServletPath(): ").append(request.getServletPath() + "\n");
+		print.append("request.getPathInfo(): ").append(request.getPathInfo() + "\n");
+	}
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//doGet(request, response);
+	}
+}
