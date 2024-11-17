5e0ddeb953c659f929ecb2133c2d356295c88bb5
ArielPazDev <contacto.arielpaz.dev@gmail.com>
2024-11-17 15:59:34 -0300

    refactor (tests): debug in CartTest

--- src/main/java/tests/CartTest.java
+++ src/main/java/tests/CartTest.java
@@ -11,40 +11,40 @@ public class CartTest {
 
 
 		// CartRepo (add)
-		cartRepo.add(new Cart(1, "LNJMM", 2, 70f));
-		cartRepo.add(new Cart(1, "OTXGM", 3, 90f));
-		cartRepo.add(new Cart(2, "L5BX5", 2, 300f));
+		cartRepo.add(new Cart(1, "LNJMM", 1, 70f));
+		cartRepo.add(new Cart(1, "OTXGM", 2, 90f));
+		cartRepo.add(new Cart(2, "L5BX5", 3, 300f));
+		cartRepo.add(new Cart(2, "NSPVQ", 4, 250f));
 
 		// CartRepo (view list)
 		cartRepo.viewList("add articles to users");
 
 
 		// CartRepo (add)
-		cartRepo.add(new Cart(1, "OTXGM", 1, 90f));
-		cartRepo.add(new Cart(2, "OTXGM", 1, 90f));
+		cartRepo.add(new Cart(1, "OTXGM", 3, 90f));
 
 		// CartRepo (view list)
-		cartRepo.viewList("add articles to users / modify quantity or new");
+		cartRepo.viewList("add 'Quantity: 3' in 'ID User: 1' with 'Code Article: OTXGM'");
 
 
 		// CartRepo (edit)
-		cartRepo.edit(new Cart(1, "LNJMM", 5, 71f));
+		cartRepo.edit(new Cart(1, "LNJMM", 6, 71f));
 
 		// CartRepo (view list)
-		cartRepo.viewList("edit article LNJMM in user 1, Quantity: 5 and Price: 71");
+		cartRepo.viewList("edit 'Quantity: 6' and 'Price: 71' in 'ID User: 1' with 'Code Article: LNJMM'");
 
 
 		// CartRepo (delete)
 		cartRepo.delete(1, "LNJMM");
 
 		// CartRepo (view list)
-		cartRepo.viewList("delete article LNJMM in user 1");
+		cartRepo.viewList("delete 'ID User: 1' with 'Code Article: LNJMM'");
 
 
 		// CartRepo (empty)
 		cartRepo.empty(2);
 
 		// CartRepo (view list)
-		cartRepo.viewList("empty user 2");
+		cartRepo.viewList("empty 'ID User: 2'");
 	}
 }
