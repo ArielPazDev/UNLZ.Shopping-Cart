aee96b44f3566f5f29abf9247725b1ba393a704c
ArielPazDev <contacto.arielpaz.dev@gmail.com>
2024-11-17 04:02:16 -0300

    feat (repos): add CartRepo

---
+++ src/main/java/repos/CartRepo.java
@@ -0,0 +1,60 @@
+package repos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import models.Cart;
+
+public class CartRepo {
+	private static CartRepo singleton;
+	
+	private List<Cart> list;
+
+	private CartRepo() {
+		this.list = new ArrayList<Cart>();
+	}
+
+	public static CartRepo getInstance() {
+		if (singleton == null ) {
+			singleton = new CartRepo();
+		}
+		
+		return singleton;
+	}
+	
+	public void add(Cart cart) {
+		Optional<Cart> cartFind = list.stream().filter(c -> c.getCodeArticle() == cart.getCodeArticle()).findFirst();
+
+		cartFind.ifPresentOrElse(c -> {
+			c.setQuantity(c.getQuantity() + cart.getQuantity());
+			c.setPrice(cart.getPrice());
+			c.setMoneySubTotal(c.getQuantity() * cart.getPrice());
+		}, () -> list.add(cart));
+	}
+
+	public void edit(Cart cart) {
+		list.stream().filter(c -> c.getCodeArticle() == cart.getCodeArticle()).forEach(c -> c.setCart(cart));
+	}
+
+	public void delete(String codeArticle) {
+		Optional<Cart> cartFind = list.stream().filter(c -> c.getCodeArticle() == codeArticle).findFirst();
+
+		cartFind.ifPresent(cart -> list.remove(cart));
+	}
+
+	public void viewList() {
+		System.out.println("List of Carts");
+		System.out.println("-------------");
+
+		IntStream.range(0, list.size())
+				.forEach(i -> System.out.println("(" + i + ") -> " + "ID User: " + list.get(i).getIdUser()
+						+ " | " + "Code Article: " + list.get(i).getCodeArticle()
+						+ " | " + "Quantity: " + list.get(i).getQuantity() + " | " + "Price: "
+						+ String.format("%.2f", list.get(i).getPrice()) + " | " + "Money SubTotal: "
+						+ String.format("%.2f", list.get(i).getMoneySubTotal())));
+
+		System.out.println("");
+	}
+}
